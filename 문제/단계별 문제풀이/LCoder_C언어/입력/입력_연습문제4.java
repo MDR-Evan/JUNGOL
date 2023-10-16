6740ecf0bdd7813d859ed0de0e875527c5c1fb67
Jinyong <kjy040216@kbu.ac.kr>
2023-10-16 17:08:15 +0900

    update

--- 문제/단계별 문제풀이/LCoder_C언어/입력/입력_연습문제4.java
+++ 문제/단계별 문제풀이/LCoder_C언어/입력/입력_연습문제4.java
@@ -1,4 +1,4 @@
-public class Main {
+public class 입력_자가진단4 {
     public static void main(String[] args) {
         System.out.printf("원주 = 5 * 2 * 3.140000 = 31.400000\n넓이 = 5 * 5 * 3.140000 = 78.500000");
     }
